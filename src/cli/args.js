daf7b10154fcadbab364ad8097d80a036095785c
Andrey Klimkov <aaklimkov@gmail.com>
2024-01-02 20:18:23 +0300

    feat: finish cli module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,28 @@
 const parseArgs = () => {
-    // Write your code here 
+  let args = process.argv.slice(2);
+
+if (args.length % 2 !== 0) {
+  console.error("Error: Arguments should be in pairs of a property and its value.");
+  process.exit(1);
+}
+
+for (let i = 0; i < args.length; i += 2) {
+  let key = args[i].replace('--', '');
+  let value = args[i + 1];
+
+  if (!args[i].startsWith('--')) {
+    console.error(`Error: The property '${args[i]}' should start with '--'.`);
+    process.exit(1);
+  }
+
+  if (value === undefined || value.startsWith('--')) {
+    console.error(`Error: Missing value for property '${key}'.`);
+    process.exit(1);
+  }
+
+  console.log(`${key} is ${value}`);
+}
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  const filteredEnvVars = Object.entries(process.env)
+  .filter(([key, _]) => key.startsWith('RSS_'));
+
+if (filteredEnvVars.length === 0) {
+  console.log("No 'RSS_' prefixed environment variables found.");
+} else {
+  filteredEnvVars.forEach(([key, value]) => {
+    console.log(`${key}=${value}`);
+  });
+}
+
 };
 
 parseEnv();
